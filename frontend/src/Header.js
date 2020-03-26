c54ec52cf40691e666eb79ac0cef003715ee2493
Pedro Henrique <pedrotelles@gec.inatel.br>
2020-03-26 13:14:44 -0300

    added mobile

---
+++ frontend/src/Header.js
@@ -0,0 +1,12 @@
+import React,{useState} from 'react';
+
+export default function Header(props){
+    return (
+        <header>
+            
+            <h1>{props.children}</h1>
+            
+        </header>
+    );
+}
+
